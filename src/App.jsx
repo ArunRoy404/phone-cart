fa0acf86eb47664679ff019a1cec06d3914661fc
ArunRoy404 <arunroy0065@gmail.com>
2025-03-31 01:08:41 +0600

    cart component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Suspense } from 'react'
 import './App.css'
 import Phones from './components/Phones/Phones'
 import { ErrorBoundary } from "react-error-boundary"
+import CartDrawer from './components/CartDrawer/CartDrawer'
 
 const url = '../public/phone.json'
 const fetchPhonesPromise = async () => {
@@ -17,10 +18,12 @@ function App() {
   return (
     <>
       <div className='bg-[#633AE4] py-9'>
-        <div className='w-[80%] mx-auto '>
+        <div className='w-[80%] mx-auto flex justify-between items-center'>
           <h1 className='text-white text-3xl font-bold'>Shop Your Favorite Phone</h1>
+          <CartDrawer></CartDrawer>
         </div>
       </div>
+      
 
       <div className='w-[80%] mx-auto mt-10 pb-10 '>
         <ErrorBoundary fallback={<h1 className='text-3xl font-bold'>Something Went Wrong</h1>}>
--- src/components/Phone/Phone.jsx
+++ src/components/Phone/Phone.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 const Phone = ({ phone }) => {
     return (
-        <div className='p-4 bg-white rounded-2xl drop-shadow-xl'>
-            {/* image */}
-            <div className='bg-slate-200 h-[170px] rounded-2xl flex justify-center items-center'>
-                <img className='h-[150px] mix-blend-darken' src={phone.img_url} alt="" />
-            </div>
-            <div className='mt-3'>
-                <h2 className='font-bold'>Model: {phone.model}</h2>
-                <h3 className='text-sm'>Brand: {phone.brand}</h3>
-                <h3 className='text-sm'>RAM: {phone.RAM}</h3>
-                <h3 className='mt-2 font-bold'>Price: ${phone.approx_price_EUR}</h3>
+        <div className='p-4 bg-white rounded-2xl drop-shadow-xl flex flex-col justify-between'>
+
+            <div>
+                <div className='bg-slate-200 h-[170px] rounded-xl flex justify-center items-center'>
+                    <img className='h-[150px] mix-blend-darken' src={phone.img_url} alt="" />
+                </div>
+                <div className='mt-3'>
+                    <h2 className='font-bold'>Model: {phone.model}</h2>
+                    <h3 className='text-sm'>Brand: {phone.brand}</h3>
+                    <h3 className='text-sm'>RAM: {phone.RAM}</h3>
+                    <h3 className='mt-2 font-bold'>Price: ${phone.approx_price_EUR}</h3>
+                </div>
             </div>
 
             <div className='mt-4 text-center'>
